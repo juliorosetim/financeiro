b535fb9939885f1f3f75312a51bf0aade63b667e
Julio Cesar Rosetim <julio.rosetim@softplan.com.br>
2023-10-02 12:09:14 -0300

    table

--- financeiro-front/src/components/CadastroCategoriaComponent.vue
+++ financeiro-front/src/components/CadastroCategoriaComponent.vue
@@ -34,7 +34,7 @@
         ></ButtonCustom>
       </div>
 
-      <div>
+      <!-- <div>
         <table>
           <thead>
             <tr>
@@ -66,6 +66,137 @@
             </tr>
           </tbody>
         </table>
+      </div> -->
+
+      <div class="m-2 h-8 w-full">
+        <p class="text-verde-50 text-xl mb-3 w-full text-center">
+          Categorias Cadastradas
+        </p>
+      </div>
+
+      <div class="flex items-center justify-center">
+        <div class="col-span-full">
+          <div class="overflow-auto lg:overflow-visible">
+            <table
+              class="table text-verde-100 border-separate space-y-6 text-sm"
+            >
+              <thead class="text-verde-100 bg-verde-50">
+                <tr>
+                  <th class="p-3">Categoria</th>
+                  <th class="p-3 text-left"></th>
+                  <th class="p-3 text-left"></th>
+                  <th class="p-3 text-left"></th>
+                  <th class="p-3 text-left"></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  class="bg-verde-50"
+                  v-for="categoria in categorias"
+                  :key="categoria.cdCategoria"
+                >
+                  <td class="p-3">
+                    <div class="flex align-items-center">
+                      <div class="ml-3">
+                        <div class="">{{ categoria.deCategoria }}</div>
+                      </div>
+                    </div>
+                  </td>
+                  <td class="p-3 flex">
+                    <span @click="exibirCategoria(categoria)"
+                      ><SvgIcon type="mdi" :path="mdiEyeOutline"></SvgIcon
+                    ></span>
+
+                    <span
+                      class="button-grid"
+                      @click="
+                        categoria.cdCategoria !== undefined
+                          ? excluirCategoria(categoria.cdCategoria)
+                          : null
+                      "
+                    >
+                      <SvgIcon type="mdi" :path="mdiDeleteOutline"></SvgIcon>
+                    </span>
+                  </td>
+                </tr>
+                <!-- <tr class="bg-gray-800">
+                  <td class="p-3">
+                    <div class="flex align-items-center">
+                      <img
+                        class="rounded-full h-12 w-12 object-cover"
+                        src="https://images.unsplash.com/photo-1423784346385-c1d4dac9893a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+                        alt="unsplash image"
+                      />
+                      <div class="ml-3">
+                        <div class="">Realme</div>
+                        <div class="text-gray-500">mail@rgmail.com</div>
+                      </div>
+                    </div>
+                  </td>
+                  <td class="p-3">Technology</td>
+                  <td class="p-3 font-bold">200.00$</td>
+                  <td class="p-3">
+                    <span class="bg-red-400 text-gray-50 rounded-md px-2"
+                      >no stock</span
+                    >
+                  </td>
+                  <td class="p-3">
+                    <a href="#" class="text-gray-400 hover:text-gray-100 mr-2">
+                      <i class="material-icons-outlined text-base"
+                        >visibility</i
+                      >
+                    </a>
+                    <a href="#" class="text-gray-400 hover:text-gray-100 mx-2">
+                      <i class="material-icons-outlined text-base">edit</i>
+                    </a>
+                    <a href="#" class="text-gray-400 hover:text-gray-100 ml-2">
+                      <i class="material-icons-round text-base"
+                        >delete_outline</i
+                      >
+                    </a>
+                  </td>
+                </tr>
+                <tr class="bg-gray-800">
+                  <td class="p-3">
+                    <div class="flex align-items-center">
+                      <img
+                        class="rounded-full h-12 w-12 object-cover"
+                        src="https://images.unsplash.com/photo-1600856209923-34372e319a5d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2135&q=80"
+                        alt="unsplash image"
+                      />
+                      <div class="ml-3">
+                        <div class="">Samsung</div>
+                        <div class="text-gray-500">mail@rgmail.com</div>
+                      </div>
+                    </div>
+                  </td>
+                  <td class="p-3">Technology</td>
+                  <td class="p-3 font-bold">200.00$</td>
+                  <td class="p-3">
+                    <span class="bg-yellow-400 text-gray-50 rounded-md px-2"
+                      >start sale</span
+                    >
+                  </td>
+                  <td class="p-3">
+                    <a href="#" class="text-gray-400 hover:text-gray-100 mr-2">
+                      <i class="material-icons-outlined text-base"
+                        >visibility</i
+                      >
+                    </a>
+                    <a href="#" class="text-gray-400 hover:text-gray-100 mx-2">
+                      <i class="material-icons-outlined text-base">edit</i>
+                    </a>
+                    <a href="#" class="text-gray-400 hover:text-gray-100 ml-2">
+                      <i class="material-icons-round text-base"
+                        >delete_outline</i
+                      >
+                    </a>
+                  </td>
+                </tr> -->
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -139,3 +270,27 @@ onMounted(() => {
   fetchCategorias();
 });
 </script>
+
+<style scoped>
+.table {
+  border-spacing: 0 5px;
+}
+
+i {
+  font-size: 1rem !important;
+}
+
+.table tr {
+  border-radius: 20px;
+}
+
+tr td:nth-child(n + 5),
+tr th:nth-child(n + 5) {
+  border-radius: 0 0.625rem 0.625rem 0;
+}
+
+tr td:nth-child(1),
+tr th:nth-child(1) {
+  border-radius: 0.625rem 0 0 0.625rem;
+}
+</style>
